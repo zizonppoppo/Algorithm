9aa60855332177e6e71da83ec82520c23942c65b
zizonppoppo <superjunwjdguswns@gmail.com>
2026-02-06 00:07:34 +0900

    실버1 11660 구간 합 구하기 5

---
+++ 백준/실버/11660번 구간 합 구하기 5/11660번 구간 합 구하기 5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<int>> sum(N + 1, vector<int>(N + 1, 0));
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            int val;
+            cin >> val;
+            sum[i][j] = val + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+        }
+    }
+
+    for (int i = 0; i < M; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1] << '\n';
+    }
+
+    return 0;
+}
